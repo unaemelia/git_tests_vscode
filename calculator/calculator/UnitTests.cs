5e78655945c0087a898821dab83cd96995cb12cf
Una Árnadóttir <unaemelia@gmail.com>
2024-01-05 10:55:08 +0100

    adding tests

--- calculator/calculator/UnitTests.cs
+++ calculator/calculator/UnitTests.cs
@@ -1,18 +1,63 @@
 using xunit;
 public class UnitTests
 {
-        public void TestAddition()
-        {
-            // given
-            Calculator Calc = new();
-            int a = 5;
-            int b = 2;
-            int expectedResult = 7;
+    public void TestAddition()
+    {
+        // given
+        Calculator Calc = new();
+        int a = 5;
+        int b = 2;
+        int expectedResult = 7;
 
-            // when
-            int result = Calc.Add(a, b);
+        // when
+        int result = Calc.Add(a, b);
 
-            // then
-            Assert.Equal(expectedResult, result);
-        }
+        // then
+        Assert.Equal(expectedResult, result);
+    }
+
+    public void TestSubtraction()
+    {
+        // given
+        Calculator Calc = new();
+        int a = 5;
+        int b = 2;
+        int expectedResult = 3;
+
+        // when
+        int result = Calc.Subtract(a, b);
+
+        // then
+        Assert.Equal(expectedResult, result);
+    }
+
+    public void TestMultiply()
+    {
+        // given
+        Calculator Calc = new();
+        int a = 5;
+        int b = 2;
+        int expectedResult = 10;
+
+        // when
+        int result = Calc.Multiply(a, b);
+
+        // then
+        Assert.Equal(expectedResult, result);
+    }
+
+    public void TestDivide()
+    {
+        // given
+        Calculator Calc = new();
+        int a = 10;
+        int b = 2;
+        int expectedResult = 5;
+
+        // when
+        int result = Calc.Division(a, b);
+
+        // then
+        Assert.Equal(expectedResult, result);
+    }
 }
